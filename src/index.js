5f5fda17030c722bb964308ab065b70a5fa6acbd
ycw <ycw7788@gmail.com>
2021-01-15 17:35:25 +0800

    solved #3

--- src/index.js
+++ src/index.js
@@ -1,122 +1,134 @@
-const DEFAULT_URL = '//unpkg.com/three/build/three.module.js';
-
-//
-// Main
-//
-
 (async function main() {
+    const url = find_module_url();
+    url ?? (location.search = '?url=//unpkg.com/three/build/three.module.js');
 
-    //// Determine threejs module url
-
-    const pars = new URLSearchParams(new URL(document.URL).search);
-    const url = pars.get('url');
-    if (url === null) {
-        return location.href = `${location.origin}${location.pathname}?url=${DEFAULT_URL}${location.hash}`;
+    const { THREE, import_err } = await import_threejs(url);
+    if (import_err) {
+        return alert(import_err.message);
     }
 
-    //// Load threejs
-
-    let THREE;
-    try {
-        THREE = await import(url);
-    } catch (e) {
-        return alert(`${e.message}`);
-    }
-
-    //// Reg events 
-
-    document.addEventListener('click', (ev) => handleCopy(THREE, ev));
-    window.addEventListener('hashchange', (ev) => handleRoute(THREE, ev));
-    document.querySelector('.expand-all-vert').addEventListener('click', () => toggleChunks('vertex', true));
-    document.querySelector('.collapse-all-vert').addEventListener('click', () => toggleChunks('vertex', false));
-    document.querySelector('.expand-all-frag').addEventListener('click', () => toggleChunks('fragment', true));
-    document.querySelector('.collapse-all-frag').addEventListener('click', () => toggleChunks('fragment', false));
-
-    //// Setup hljs ( https://github.com/highlightjs/highlight.js/issues/2559 )
-
-    const brPlugin = {
-        "before:highlightBlock": ({ block }) => {
-            block.innerHTML = block.innerHTML.replace(/\n/g, '').replace(/<br[ /]*>/g, '\n');
-        },
-        "after:highlightBlock": ({ result }) => {
-            result.value = result.value.replace(/\n/g, "<br>");
-        }
-    };
-    hljs.addPlugin(brPlugin);
-
-    //// Render page 
-
-    const shaderName = location.hash.substr(1);
-    const shaderObject = THREE.ShaderLib[shaderName];
-    if (!shaderObject) {
-        const defaultShaderName = Object.keys(THREE.ShaderLib)[0];
-        return location.hash = defaultShaderName;
-    }
-    render(THREE, shaderName);
-
+    reg_events(THREE, location);
+    route(THREE, location);
 })();
+
+function find_module_url() {
+    return new URLSearchParams(location.search).get('url');
+}
+
+async function import_threejs(url) {
+    try {
+        return { THREE: await import(url) };
+    } catch (import_err) {
+        return { import_err };
+    }
+}
+
+function reg_events(THREE, location) {
+    document.addEventListener('click', ev => maybe_click_expand_all(ev));
+    document.addEventListener('click', ev => maybe_click_collapse_all(ev));
+    document.addEventListener('click', ev => maybe_click_copy(THREE, ev));
+    document.addEventListener('click', ev => maybe_click_include(ev));
+    document.addEventListener('click', ev => maybe_click_end_tag(ev));
+    window.addEventListener('hashchange', () => route(THREE, location));
+}
+
+function route(THREE, locaiton) {
+    const shader_name = location.hash.substr(1);
+    THREE.ShaderLib[shader_name]
+        ? render(THREE, shader_name)
+        : (location.hash = Object.keys(THREE.ShaderLib)[0]);
+}
 
 //
 // Event handlers
 //
 
-function handleRoute(THREE, ev) {
-    const shaderName = location.hash.substr(1);
-    const shaderObject = THREE.ShaderLib[shaderName];
-    if (!shaderObject) {
-        const defaultShaderName = Object.keys(THREE.ShaderLib)[0];
-        return location.hash = defaultShaderName;
-    }
-    render(THREE, shaderName);
-}
-
-function handleCopy(THREE, ev) {
-    const el = ev.composedPath().find(el => el.classList?.contains('copy'));
-    if (el) {
-        const chunkName = el.closest('details').dataset.shaderchunk;
-        const chunkContent = THREE.ShaderChunk[chunkName];
-        const textToCopy = chunkContent.replace(/\t/gm, ' '.repeat(4));
-        navigator.clipboard
-            .writeText(textToCopy)
-            .then(() => {
-                const oText = el.textContent;
-                el.textContent = 'copied';
-                setTimeout(() => el.textContent = oText, 1500);
-            });
+function maybe_click_include(ev) {
+    const summary = ev.composedPath().find(el => el.nodeName == 'SUMMARY');
+    if (summary) {
+        const details = summary.closest('details');
+        toggle_include(details, !details.hasAttribute('open'));
         ev.preventDefault();
     }
 }
 
-function toggleChunks(kind, isToggle) {
+function maybe_click_expand_all(ev) {
+    ev.target.classList.contains('expand-all')
+        && toggle_all_includes(ev.target.dataset.kind, true);
+}
+
+function maybe_click_collapse_all(ev) {
+    ev.target.classList.contains('collapse-all')
+        && toggle_all_includes(ev.target.dataset.kind, false);
+}
+
+function toggle_include(details, is_expand) {
+    const summary = details.querySelector('summary');
+    const i = summary.querySelector('i');
+    i.textContent = is_expand ? '-' : '+';
+    hljs.highlightBlock(summary);
+    summary.classList.remove('hljs');
+    details.toggleAttribute('open', is_expand);
+}
+
+function toggle_all_includes(kind, is_expand) {
     const el = document.querySelector(`.${kind}`);
-    Array.from(el.querySelectorAll('details')).forEach(el => el.toggleAttribute('open', isToggle));
+    el.querySelectorAll('details').forEach($ => toggle_include($, is_expand));
     el.scrollIntoView();
+}
+
+function maybe_click_copy(THREE, ev) {
+    const el = ev.composedPath().find(el => el.classList?.contains('copy'));
+    if (el) {
+        const chunk = el.closest('details').dataset.chunk;
+        const text = THREE.ShaderChunk[chunk].replace(/\t/gm, ' '.repeat(4));
+        navigator.clipboard.writeText(text).then(() => {
+            const old = el.textContent;
+            el.textContent = 'copied';
+            setTimeout(() => el.textContent = old, 1500);
+        });
+        ev.preventDefault();
+        ev.stopImmediatePropagation();
+    }
+}
+
+function maybe_click_end_tag(ev) {
+    const el = ev.composedPath().find(el => el.classList?.contains('end-tag'));
+    if (el) {
+        const details = el.closest('details');
+        toggle_include(details, false);
+    }
 }
 
 //
 // Render fns
 //
 
-function render(THREE, shaderName) {
+function render(THREE, shader_name) {
     document.body.classList.add('render');
-    renderShaderList(THREE.ShaderLib, THREE.REVISION, shaderName);
-    renderShader(shaderName, THREE.ShaderLib[shaderName], THREE.ShaderChunk);
+    render_shader_list(THREE.ShaderLib, THREE.REVISION, shader_name);
+    render_shader(shader_name, THREE.ShaderLib[shader_name], THREE.ShaderChunk);
     document.querySelector('.vertex').scrollIntoView();
 }
 
-function renderShaderList(ShaderLib, revision, shaderName) {
-    document.querySelector('.shader-list').innerHTML = tmplShaderList(ShaderLib, revision, shaderName);
+function render_shader_list(ShaderLib, revision, shaderName) {
+    document.querySelector('.shader-list').innerHTML =
+        tmpl_shader_list(ShaderLib, revision, shaderName);
 }
 
-function renderShader(shaderName, shaderObject, ShaderChunk) {
+function render_shader(shaderName, shaderObject, ShaderChunk) {
     document.querySelector('.name').innerHTML = shaderName;
-    document.querySelector('.uniforms').innerHTML = tmplUniforms(shaderObject.uniforms);
-    document.querySelector('.vertex').innerHTML = tmplShaderSource(shaderObject.vertexShader, ShaderChunk);
-    document.querySelector('.fragment').innerHTML = tmplShaderSource(shaderObject.fragmentShader, ShaderChunk);
 
-    //// highlight glsl code
+    document.querySelector('.uniforms').innerHTML =
+        tmpl_uniforms(shaderObject.uniforms);
 
-    Array.from(document.querySelectorAll('code.language-glsl'))
+    document.querySelector('.vertex').innerHTML =
+        tmpl_shader_source(shaderObject.vertexShader, ShaderChunk);
+
+    document.querySelector('.fragment').innerHTML =
+        tmpl_shader_source(shaderObject.fragmentShader, ShaderChunk);
+
+    document.querySelectorAll('code.language-glsl')
         .forEach(el => hljs.highlightBlock(el));
 }
 
@@ -124,54 +136,57 @@ function renderShader(shaderName, shaderObject, ShaderChunk) {
 // Templates
 //
 
-function tmplShaderList(ShaderLib, revision, selectedShaderName) {
+function tmpl_shader_list(ShaderLib, revision, selected) {
     return `
     <h1>ShaderLib r${revision}</h1>
-    <ul>${Object.keys(ShaderLib).map(shaderName => {
-        const classAttr = shaderName === selectedShaderName ? 'class="selected"' : '';
-        return `<li ${classAttr}><a href='#${shaderName}'>${shaderName}</a></li>`
-    }).join('')}</ul>
+    <ul>${Object.keys(ShaderLib).map(shader_name =>
+        `<li><a href='#${shader_name}'>${shader_name === selected
+            ? `<mark>${shader_name}</mark>`
+            : shader_name
+        }</a></li>`
+    ).join('')}</ul>
     `;
 }
 
-function tmplUniforms(uniforms) {
+function tmpl_uniforms(uniforms) {
     const uniformNames = Object.keys(uniforms);
-    return `<ul class='shader-uniforms'>
-        ${uniformNames.map(u => `<li><a>${u}</a></li>`).join('')}
-    </ul>`;
+    return `${uniformNames.map(u => `<li><a>${u}</a></li>`).join('')}`;
 }
 
-function tmplShaderSource(shaderSource, ShaderChunk) {
-    const html = shaderSourceToHtml(shaderSource, ShaderChunk);
-    return `<code class='language-glsl'>${html}</code>`;
+function tmpl_shader_source(shaderSource, ShaderChunk) {
+    const html = shader_source_to_html(shaderSource, ShaderChunk);
+    return `<pre><code class='language-glsl'>${html}</code></pre>`;
 }
 
 //
-// Shader Source -> HTML
+// Shader source -> html
 //
 
-function shaderSourceToHtml(shaderSource, ShaderChunk, indent = '') {
-    const lines = shaderSource.trim().split('\n');
+function shader_source_to_html(src, ShaderChunk, indents = '') {
+    const lines = src.trim().split('\n');
     const html = [];
-    for (const [i, line] of lines.entries()) {
-        const match = line.match(/^(?<indent>\s*)#include\s+<\s*(?<chunk>.+?)\s*>$/);
+    const re = /^(?<indent>\s*)#include\s+<\s*(?<chunk>.+?)\s*>$/;
+    for (const line of lines) {
+        const match = line.match(re);
         if (match) {
             const { chunk, indent } = match.groups;
-            html.push(`<details data-shaderchunk='${chunk}'>`);
-            html.push(`<summary>${indent}// &lt;${chunk}&gt;\
-<div class='actions'>\
-<a class='copy' href='#' title='Write &lt;${chunk}&gt; content to clipboard'>Copy</a>\
-</div></summary>`);
-            html.push(`<br/>`);
-            html.push(shaderSourceToHtml(ShaderChunk[chunk], ShaderChunk, indent));
-            html.push('<br/>');
+            const ii = indents + indent;
+            html.push(`<details data-chunk='${chunk}'>`);
+            html.push(`<summary>${ii}//<i>+</i> &lt;${chunk}&gt;`);
+            html.push(`<a class='copy' title='Write to clipboard'>Copy</a>`);
+            html.push(`</summary>`);
+            html.push('\n');
+            html.push(shader_source_to_html(ShaderChunk[chunk], ShaderChunk, ii));
+            html.push('\n');
+            html.push(`${ii}<a class='end-tag'>//- &lt;/${chunk}&gt;</a>`);
+            html.push('\n');
             html.push('</details>');
         }
         else {
-            html.push(indent + line.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
-            html.push('<br/>');
+            html.push(indents);
+            html.push(line.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+            html.push('\n');
         }
     }
-
     return html.join('');
 }
