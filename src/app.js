a708606b0defff2f82429bfd6613ba2f27f53b49
ycw <ycw7788@gmail.com>
2021-01-21 09:21:49 +0800

    isolate defaults

--- src/app.js
+++ src/app.js
@@ -33,24 +33,23 @@ export const app = {
     },
 
     route: (hash) => {
-        const { ShaderLib, ShaderChunk, REVISION } = app.state.THREE;
-        const shader_name = router.get_shader_name(hash) || Object.keys(ShaderLib)[0];
-        const info_name = router.get_info_name(hash) || 'vertex';
+        const shader_name = router.get_shader_name(hash) || app.default_shader_name;
+        const info_name = router.get_info_name(hash) || app.default_info_name;
         if (app.validate_query(shader_name, info_name)) {
-            if (!router.is_route_qualified(hash)) {
-                router.redirect(router.get_module_version(hash), shader_name, info_name);
-            }
+            const { ShaderLib, ShaderChunk, REVISION } = app.state.THREE;
             view.init_render(ShaderLib, ShaderChunk, REVISION, shader_name, info_name);
+            router.redirect(router.get_module_version(hash), shader_name, info_name);
         }
     },
 
     validate_query: (shader_name, info_name) => {
         const errors = [];
+
         if (!app.state.THREE) {
             errors.push('Threejs is not loaded');
         }
 
-        if (!Object.hasOwnProperty.call(app.state.THREE.ShaderLib, shader_name)) {
+        if (!Object.prototype.hasOwnProperty.call(app.state.THREE.ShaderLib, shader_name)) {
             errors.push(`Shader("${shader_name}") is not in ShaderLib`);
         }
 
@@ -66,5 +65,13 @@ export const app = {
 
         dom.el('body').classList.add('valid-query');
         return true;
+    },
+
+    get default_shader_name() {
+        return Object.keys(app.state.THREE.ShaderLib)[0];
+    },
+
+    get default_info_name() {
+        return 'vertex';
     }
 };
